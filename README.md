1eb0ad25435117c58b9131b28fdb9ba995e290df
023012 <ntd.dat1230@gmail.com>
2023-01-05 16:55:51 +0700

    new spring boot crud_demo

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 "# SpringBoot2023" 
+1. crud_demo01 : 
+application srping boot crud basic 
+	depencency:
+		- Spring Web
+		- Mysql Database
+		- Lombok
+		- Spring Data Jpa
---
+++ crud_demo01/src/test/java/com/thdat/crud_demo01/CrudDemo01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thdat.crud_demo01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudDemo01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
